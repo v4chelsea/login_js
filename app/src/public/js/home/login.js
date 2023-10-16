8344d4b402cd064fc273b0d0d0006172fdac1b46
marin <marin@studiolab.ai>
2023-10-16 18:48:39 +0900

    feat: add event listener

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,18 @@
 "use strict";
 
-console.log("Hello world");
+// DOM Document Object Model / HTML에서 객체를 컨트롤 할 수 있게 해줌
+
+const id = document.querySelector("#id"),
+  password = document.querySelector("#password"),
+  login_btn = document.querySelector("button") 
+
+login_btn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : id.value,
+        password : password.value
+    };
+
+    fetch()l
+};
--- src/bin/www.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const app = require("../app");
-const PORT = 3000;
-
-// 서버 실행 
-app.listen(PORT, () => {
-    console.log("Hello Server"); 
-});  
